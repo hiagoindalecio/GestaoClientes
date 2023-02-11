f64ffb7936346995eefb3140314ad2e0297b6720
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2023-02-11 11:30:53 -0300

    Arrumando procedures para a atualização de clientes e beneficiários de forma atômica

--- FI.AtividadeEntrevista/DAL/Clientes/Procedures/FI_SP_AltCliente.sql
+++ FI.AtividadeEntrevista/DAL/Clientes/Procedures/FI_SP_AltCliente.sql
@@ -8,8 +8,9 @@
     @CIDADE        VARCHAR (50) ,
     @LOGRADOURO    VARCHAR (500),
     @EMAIL         VARCHAR (2079),
-    @TELEFONE      VARCHAR (15),
-	@Id           BIGINT
+    @TELEFONE      VARCHAR (15) ,
+	@ID            BIGINT       ,
+	@BENEFICIARIOS BeneficiariosList NULL READONLY
 AS
 BEGIN
 	UPDATE CLIENTES 
@@ -24,5 +25,51 @@ BEGIN
 		LOGRADOURO = @LOGRADOURO, 
 		EMAIL = @EMAIL, 
 		TELEFONE = @TELEFONE
-	WHERE Id = @Id
+	WHERE ID = @ID
+
+    -- Busca beneficiários já cadastrados para este cliente
+    DECLARE @BeneficiariosExistentes BeneficiariosList
+    INSERT INTO @BeneficiariosExistentes (ID, CPF, NOME)
+    SELECT ID, CPF, NOME
+    FROM BENEFICIARIOS
+    WHERE IDCLIENTE = @ID
+
+    -- Variáveis auxiliares
+    DECLARE @IdBen BIGINT = 0;
+    DECLARE @CpfBen VARCHAR (14) = ''
+    DECLARE @NomeBen VARCHAR (50) = ''
+
+	-- Cria e atualiza beneficiarios
+    DECLARE cursor_ben_recebidos CURSOR FOR
+        SELECT ID, CPF, NOME
+        FROM @BENEFICIARIOS
+
+    OPEN cursor_ben_recebidos  
+    FETCH NEXT FROM cursor_ben_recebidos INTO @IdBen, @CpfBen, @NomeBen
+
+    WHILE @@FETCH_STATUS = 0  
+    BEGIN
+        IF (@IdBen = 0)
+            EXEC dbo.FI_SP_IncBeneficiario @NOME = @NomeBen, @CPF = @CpfBen, @IDCLIENTE = @ID;
+        ELSE
+            EXEC dbo.FI_SP_AltBeneficiario @NOME = @NomeBen, @CPF = @CpfBen, @ID = @IdBen;
+
+        FETCH NEXT FROM cursor_ben_recebidos INTO @IdBen, @CpfBen, @NomeBen
+    END
+
+    -- Deleta beneficiarios
+    DECLARE cursor_ben_existentes CURSOR FOR
+        SELECT ID
+        FROM @BeneficiariosExistentes
+        WHERE ID NOT IN (SELECT ID FROM @BENEFICIARIOS)
+
+    OPEN cursor_ben_existentes  
+    FETCH NEXT FROM cursor_ben_existentes INTO @IdBen
+
+    WHILE @@FETCH_STATUS = 0  
+    BEGIN
+        EXEC dbo.FI_SP_DelBeneficiario @ID = @IdBen;
+
+        FETCH NEXT FROM cursor_ben_recebidos INTO @IdBen
+    END
 END
