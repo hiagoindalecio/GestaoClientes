279dce178d1b81cefaf911617b28d82f0f94765d
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2023-02-12 12:30:13 -0300

    Implementando visualmente a edição de beneficiários

--- FI.WebAtividadeEntrevista/Scripts/Beneficiarios/FI.Beneficiarios.js
+++ FI.WebAtividadeEntrevista/Scripts/Beneficiarios/FI.Beneficiarios.js
@@ -4,48 +4,119 @@
 
     formBeneficiarios = {
         listaBeneficiarios: beneficiariosExistentes,
-        functions: {
-            salvar: (cpf, nome) => {
+        refCpfEmEdicao: '',
+        funcoes: {
+            salvar: (id, cpf, nome) => {
                 if (!validaCpf(cpf))
                     ModalDialog("Ocorreu um erro", "O CPF informado é inválido.");
+                else if (id === '')
+                    formBeneficiarios.lista.funcoes.include(cpf, nome);
                 else
-                    formBeneficiarios.lista.functions.include(cpf, nome);
+                    formBeneficiarios.lista.funcoes.update(id, cpf, nome);
             },
             limpar: () => {
+                $('#IdBeneficiario').val('');
                 $('#CPFBeneficiario').val('');
                 $('#NomeBeneficiario').val('');
+                formBeneficiarios.refCpfEmEdicao = '';
             }
         },
         lista: {
-            functions: {
+            funcoes: {
+                encontrarIndicePorCpf: (cpf) =>
+                    formBeneficiarios.listaBeneficiarios.findIndex(ben => ben.CPF === cpf || ben.CPF.replace(/[^0-9]/g, '') === cpf),
                 include: (cpf, nome) => {
-                    const cpfSomenteNum = cpf.replace(/[^0-9]/g, '');
-
-                    if ($(`#${cpfSomenteNum}`).length)
+                    if (formBeneficiarios.lista.funcoes.encontrarIndicePorCpf(cpf) !== -1)
                         ModalDialog("Ocorreu um erro", "O CPF informado já foi cadastrado.");
                     else {
                         $('#gridBeneficiarios').find('#gridBeneficiariosBody')
-                            .append(`<tr id="${cpfSomenteNum}">
-                                        <td>${cpf}</td>
-                                        <td>${nome}</td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary" onclick="formBeneficiarios.lista.functions.alterar('${cpfSomenteNum}')">Alterar</button>
-                                            <button type="button" class="btn btn-primary" onclick="formBeneficiarios.lista.functions.delete('${cpfSomenteNum}')">Excluir</button>
-                                        </td>
-                                    </tr>`);
+                            .append(formBeneficiarios.lista.funcoes.montarLinha(cpf, nome));
 
-                        formBeneficiarios.listaBeneficiarios.push({ id: 0, cpf, nome });
+                        formBeneficiarios.listaBeneficiarios.push({ Id: 0, CPF: cpf, Nome: nome });
 
-                        formBeneficiarios.functions.limpar();
+                        formBeneficiarios.funcoes.limpar();
                     }
                 },
                 delete: (cpf) => {
                     $(`#${cpf}`).remove();
                     formBeneficiarios.listaBeneficiarios
-                        .splice(formBeneficiarios.listaBeneficiarios.findIndex(ben => ben.CPF.replace(/[^0-9]/g, '') === cpf), 1);
+                        .splice(formBeneficiarios.lista.funcoes.encontrarIndicePorCpf(cpf), 1);
                 },
-                alterar: (cpf) => {
-                    console.log(cpf);
+                update: (id, cpf, nome, cpfAnterior) => {
+                    const indicePorCpf = formBeneficiarios.lista.funcoes.encontrarIndicePorCpf(cpf);
+
+                    if (indicePorCpf !== -1 &&
+                        formBeneficiarios.listaBeneficiarios[indicePorCpf].Id.toString() !== id) // Encontrou CPF igual em um beneficiário diferente do que está sendo editado
+                        ModalDialog("Ocorreu um erro", "O CPF informado já foi cadastrado.");
+                    else {
+                        // Atualiza item na lista do modal
+                        const itemLista = $(`#${formBeneficiarios.refCpfEmEdicao}`);
+                        $(itemLista).find(`td[name='cpf']`).html(cpf);
+                        $(itemLista).find(`td[name='nome']`).html(nome);
+                        $(itemLista).find(`button[name='alterar-beneficiario']`)
+                            .attr('onclick', `formBeneficiarios.lista.funcoes.modoEdicao('${cpf.replace(/[^0-9]/g, '')}')`);
+                        $(itemLista).attr('id', cpf.replace(/[^0-9]/g, ''));
+
+                        // Atualiza lista de beneficiários salvos no formulário
+                        const indicePorCpfExistente = formBeneficiarios.lista.funcoes.encontrarIndicePorCpf(formBeneficiarios.refCpfEmEdicao);
+                        formBeneficiarios.listaBeneficiarios[indicePorCpfExistente].CPF = cpf;
+                        formBeneficiarios.listaBeneficiarios[indicePorCpfExistente].Nome = nome;
+
+                        formBeneficiarios.funcoes.limpar();
+                        formBeneficiarios.lista.funcoes.modoAdicao();
+                    }
+                },
+                montarLinha: (cpf, nome) => {
+                    const cpfSomenteNum = cpf.replace(/[^0-9]/g, '');
+
+                    return `<tr id="${cpfSomenteNum}">
+                                <td name="cpf">${cpf}</td>
+                                <td name="nome">${nome}</td>
+                                <td>
+                                    <span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip">
+                                        <button type="button"
+                                            name="alterar-beneficiario"
+                                            class="btn btn-primary"
+                                            onclick="formBeneficiarios.lista.funcoes.modoEdicao('${cpfSomenteNum}')">
+                                            Alterar
+                                        </button>
+                                    </span>
+                                    <span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip">
+                                        <button type="button"
+                                            class="btn btn-primary"
+                                            onclick="formBeneficiarios.lista.funcoes.delete('${cpfSomenteNum}')">
+                                            Excluir
+                                        </button>
+                                    </span>
+                                </td>
+                            </tr>`
+                },
+                modoEdicao: (cpf) => {
+                    const index = formBeneficiarios.lista.funcoes.encontrarIndicePorCpf(cpf);
+                    if (index > -1) {
+                        $('#IdBeneficiario').val(formBeneficiarios.listaBeneficiarios[index].Id);
+                        $('#CPFBeneficiario').val(formBeneficiarios.listaBeneficiarios[index].CPF);
+                        $('#NomeBeneficiario').val(formBeneficiarios.listaBeneficiarios[index].Nome);
+                        formBeneficiarios.refCpfEmEdicao = formBeneficiarios.listaBeneficiarios[index].CPF.replace(/[^0-9]/g, '');
+                        formBeneficiarios.lista.funcoes.bloquear();
+                        $('#btn-incluir-beneficiario').html('Atualizar');
+                    }
+                },
+                modoAdicao: () => {
+                    formBeneficiarios.lista.funcoes.desbloquear();
+                    $('#btn-incluir-beneficiario').html('Incluir');
+                },
+                bloquear: () => {
+                    $('#gridBeneficiarios').find('#gridBeneficiariosBody button')
+                        .attr('disabled', true)
+                        .parent()
+                        .attr('title', 'Por favor, finalize a edição');
+                },
+                desbloquear: () => {
+                    $('#gridBeneficiarios').find('#gridBeneficiariosBody button')
+                        .attr('disabled', false)
+                        .parent()
+                        .removeAttr('title');
                 }
             }
         }
@@ -54,21 +125,15 @@
     if (formBeneficiarios.listaBeneficiarios.length > 0)
         formBeneficiarios.listaBeneficiarios.forEach(b =>
             $('#gridBeneficiarios').find('#gridBeneficiariosBody')
-                .append(`<tr id="${b.CPF.replace(/[^0-9]/g, '')}">
-                            <td>${b.CPF}</td>
-                            <td>${b.Nome}</td>
-                            <td>
-                                <button type="button" class="btn btn-primary" onclick="formBeneficiarios.lista.functions.alterar('${b.CPF.replace(/[^0-9]/g, '')}')">Alterar</button>
-                                <button type="button" class="btn btn-primary" onclick="formBeneficiarios.lista.functions.delete('${b.CPF.replace(/[^0-9]/g, '')}')">Excluir</button>
-                            </td>
-                        </tr>`));
+                .append(formBeneficiarios.lista.funcoes.montarLinha(b.CPF, b.Nome)));
 
     $('#btn-incluir-beneficiario').on('click', function (e) {
         e.preventDefault();
 
+        const id = $('#IdBeneficiario').val();
         const cpf = $('#CPFBeneficiario').val();
         const nome = $('#NomeBeneficiario').val();
 
-        formBeneficiarios.functions.salvar(cpf, nome);
+        formBeneficiarios.funcoes.salvar(id, cpf, nome);
     });
 });
